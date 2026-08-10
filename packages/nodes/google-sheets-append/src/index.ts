e91b26b621e0e8ddbb2b838e504b1545d12f9502
victor santiago <victorhenriquedejesussantiago@gmail.com>
2026-08-10 10:14:06 -0300

    feat: add Google Sheets append node with runtime logic and request handling

---
+++ packages/nodes/google-sheets-append/src/index.ts
@@ -0,0 +1,9 @@
+export { googleSheetsAppendNode } from './runtime.js';
+export { googleSheetsAppendParamsSchema, type GoogleSheetsAppendParams } from './schema.js';
+export {
+  buildAppendUrl,
+  buildAppendPayload,
+  buildRequest,
+  type AppendPayload,
+  type BuiltRequest,
+} from './request.js';
---
+++ packages/nodes/http-request/src/index.ts
@@ -0,0 +1,3 @@
+export { httpRequestNode } from './runtime.js';
+export { httpRequestParamsSchema, HTTP_METHODS, type HttpRequestParams } from './schema.js';
+export { buildUrl, buildRequest, parseResponse } from './request.js';
